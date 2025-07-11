20b1a766f96fbca24b0b26e2e4c35eef591fec77
MBaarrunbinjamal <mbaarrun2008@gmail.com~>
2025-07-11 17:38:25 +0500

    op

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+// user routes start
+Route::get('/', function () {
+    return view('clients.index');
+});
+Route::get('/about', function () {
+    return view('clients.about');
+});
+Route::get('/blog-single', function () {
+    return view('clients.blog-single');
+});
+Route::get('/blog', function () {
+    return view('clients.blog');
+});
+Route::get('/contact', function () {
+    return view('clients.contact');
+});
+Route::get('/counselor', function () {
+    return view('clients.counselor');
+});
+Route::get('/main', function () {
+    return view('clients.main');
+});
+Route::get('/nav', function () {
+    return view('clients.navbar');
+});
+Route::get('/pricing', function () {
+    return view('clients.pricing');
+});
+Route::get('/service', function () {
+    return view('clients.services');
+});
+// clients routes end
